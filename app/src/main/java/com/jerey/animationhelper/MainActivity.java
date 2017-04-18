2907b29036bccf95e7b9983821c4ea13dc533e38
Xiamin <610315802@qq.com>
2017-04-18 23:18:10 +0800

    增加Activity跳转界面过渡动画使用demo

--- app/src/main/java/com/jerey/animationhelper/MainActivity.java
+++ app/src/main/java/com/jerey/animationhelper/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jerey.animationhelper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageView mImageView;
     Button mStartBtn;
     Button mHideBtn;
+    Button mStartAvtivityBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +23,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mImageView = (ImageView) findViewById(R.id.img);
         mStartBtn = (Button) findViewById(R.id.show);
         mHideBtn = (Button) findViewById(R.id.hide);
+        mStartAvtivityBtn = (Button) findViewById(R.id.startactivity);
         mStartBtn.setOnClickListener(this);
         mHideBtn.setOnClickListener(this);
+        mStartAvtivityBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-       switch (v.getId()){
-           case R.id.show:
-               AnimationHelper.show(mImageView);
-               break;
-           case R.id.hide:
-               AnimationHelper.hide(mImageView);
-               break;
-       }
+        switch (v.getId()) {
+            case R.id.show:
+                AnimationHelper.show(mImageView);
+                break;
+            case R.id.hide:
+                AnimationHelper.hide(mImageView);
+                break;
+            case R.id.startactivity:
+                AnimationHelper.startActivity(MainActivity.this,
+                        new Intent(MainActivity.this, LoginActivity.class),
+                        mStartAvtivityBtn,
+                        R.color.colorPrimary
+                );
+                break;
+        }
     }
 }
